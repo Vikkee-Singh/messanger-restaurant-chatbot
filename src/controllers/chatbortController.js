c4738b4fdba39dc0865aca016fb56763075a9077
VIKKEE SINGH <vikkee@apples-MacBook-Air-3.local>
2021-04-04 14:26:38 +0530

    update handle postback

--- src/controllers/chatbortController.js
+++ src/controllers/chatbortController.js
@@ -115,9 +115,8 @@ function handleMessage(sender_psid, received_message) {
   }
 
 // Handles messaging_postbacks events
-async function handlePostback(sender_psid, received_postback) {
+let handlePostback = async (sender_psid, received_postback) => {
     let response;
-  
     // Get the payload for the postback
     let payload = received_postback.payload;
 
